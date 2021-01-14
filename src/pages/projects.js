1aeca1ee2709f413c3db7147f27ed65c0a9dc0c3
dlomedes <diomedes.lajara@gmail.com>
2021-01-14 16:41:13 -0500

    added component projects to a page

---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../layout"
+import Work from "../layout/Work"
+
+export default function projects() {
+  return (
+    <Layout>
+      <Work />
+    </Layout>
+  )
+}
